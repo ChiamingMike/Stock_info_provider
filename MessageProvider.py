91a145fa2fbc17a3edba9e44361138b0756405e1
ChiamingMike <yea032220001122@yahoo.co.jpgit config --global user.name ChiamingMikegit --globalgit config --global user.email yea032220001122@yahoo.co.jp>
2020-11-15 09:19:10 +0900

    UPDATE changed the dataframes output style

--- MessageProvider.py
+++ MessageProvider.py
@@ -17,22 +17,25 @@ def root():
 
 @app.route('/search_result', methods=['GET', 'POST'])
 def search_result():
-    df = pd.DataFrame({"col1": ["val1", "val2", "val3"]}, index=[
-                      "row1", "row2", "row3"])
     input_symbol = request.form["Symbol"]
     info_center = StockInfoCenter(input_symbol)
     symbol_msg_info = info_center.tell_me()
     profile = symbol_msg_info.get("profile")
-    recent_five_years = symbol_msg_info.get("Recent five years")
+    recent_years = symbol_msg_info.get("Recent five years")
     average_price = symbol_msg_info.get("Average price")
     log.i(profile)
-    log.i(recent_five_years)
+    log.i(recent_years)
     log.i(average_price)
     return render_template("home.html",
                            input_symbol=input_symbol,
-                           tb1=profile.to_html(header=True),
-                           tb2=recent_five_years.to_html(header=True),
-                           tb3=average_price.to_html(header=True))
+                           tb1=profile.to_html(header=True,
+                                               index=None,
+                                               justify="center",
+                                               table_id="table1"),
+                           tb2=recent_years.to_html(header=True,
+                                                    table_id="table2"),
+                           tb3=average_price.to_html(header=True,
+                                                     table_id="table3"))
 
 
 if __name__ == "__main__":
